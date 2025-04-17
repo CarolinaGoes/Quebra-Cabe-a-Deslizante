6f898982acccf7510631f03c7fe44a81ccaf7747
CarolinaGoes <carolinarocha.89@hotmail.com>
2025-04-17 15:02:23 -0300

    .

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let img = document.querySelector("#img");
 
+const botaoImagem = document.getElementById('mudaImagem');
+const imagem = document.querySelector('#img img');
+
+botaoImagem.addEventListener('click', () => {
+  const novaUrl = `https://picsum.photos/200/200?random=${Math.floor(Math.random() * 1000)}`;
+  imagem.src = novaUrl;
+});
